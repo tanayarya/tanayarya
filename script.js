f5be12a8e0d1a5333ce5959c620ca78437b87788
Tanay Arya <62894731+tanayarya@users.noreply.github.com>
2021-12-30 00:26:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+// Nested If else
+
+// const day = prompt('Enter day');
+
+// if (day === 'Monday')
+// {
+//         console.log('Workout');
+//     }
+// else if (day === 'Tuesday' || day === 'Wednesday')
+// {
+//     console.log('Successfullll!')
+
+// }
+// else if (day === 'Saturday')
+// {
+//     console.log('Enjoy Weekend!')
+// }
+
+// Dollar Use
+
+// let name = 'Tanay', Age = 20;
+// console.log (`I am ${name} and my age is ${Age} years`)
+
+// The Conditional Operator
+
+// let age =12
+
+// age >= 18 ? console.log('Fucking allowed') : console.log('Sorry Kido!');
+
+// const drink = age >= 18 ? 'Alloted' : 'Better Luck Next time';
+// console.log(drink);
+
+// console.log(`Any piece of text ${age >= 18 ? 'Alloted' : 'Better Luck Next time'}`)
+
+// Challange 5
+
+// let bill = 400 ;
+
+// const total = bill <= 300 && bill >= 50 ? bill = bill + 15 : bill = bill+20 ;
+
+// console.log(total);
+
+// Create Functions
+
+// "use strict";
+
+// function addition(no1,no2)
+// {
+
+//     total = no1 + no2;
+
+//     return total;
+// }
+
+// console.log(addition(5,10));
+
+// function add(no1,no2)
+// {
+
+//     total = no1 + no2;
+//     console.log(total);
+// }
+
+// add(51,110);
+
+// Function Calling onother Function
+// function cutPieces(piece) {
+//   return piece * 4;
+// }
+
+// function mixer(mango, chocolate) {
+//     const mangoPiece = cutPieces(mango);
+//     const chocolatePiece = cutPieces(chocolate);
+
+//   console.log(`Juice with ${mangoPiece} pieces and ${chocolatePiece} is Ready`);
+// }
+
+// mixer(4, 8);
+
+// Arrow Function
+
+// const calAge = birthYear => 2034 - birthYear;
+
+// Function Coding Challange 1
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+
+// function checkWinner(avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log("No team wins...");
+//   }
+// }
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// Array Data Structure
+// const friends = ["tanay", "anushka", "amayra", "asmi"];
+// console.log(friends[0]);
+// console.log(friends.length);
